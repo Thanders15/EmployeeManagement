ddc918dfaf8bf74335b462fedb5ad9a37234483a
Thanders15 <adam.podoluch1999@gmail.com>
2021-10-13 16:18:44 +0200

    Deleting crashed angular

--- AngularEmployee/karma.conf.js
+++ AngularEmployee/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/FrontEmployee'),
+      dir: require('path').join(__dirname, './coverage/AngularEmployee'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- AngularEmployee/src/app/app.component.spec.ts
+++ AngularEmployee/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FrontEmployee'`, () => {
+  it(`should have as title 'AngularEmployee'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FrontEmployee');
+    expect(app.title).toEqual('AngularEmployee');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FrontEmployee app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularEmployee app is running!');
   });
 });
